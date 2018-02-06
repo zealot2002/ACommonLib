a867a027cdbfa28e4468512275094f581f83e13d
zzy <zzy_2002@126.com>
2018-02-06 09:48:19 +0800

    delete all res

--- app/src/main/java/com/zzy/acommonlib/MainActivity.java
+++ app/src/main/java/com/zzy/acommonlib/MainActivity.java
@@ -1,24 +1,9 @@
 package com.zzy.acommonlib;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.zzy.commonlib.http.HConstant;
-import com.zzy.commonlib.http.HInterface;
-import com.zzy.commonlib.http.HProxy;
-import com.zzy.commonlib.http.RequestCtx;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.util.LinkedHashMap;
-
-import static com.zzy.commonlib.http.HConstant.HTTP_METHOD_GET;
 
 public class MainActivity extends Activity {
 
