c7e6d2d3aa9353fe31448f66d4795d0bb3287f18
zzy <zzy_2002@126.com>
2018-04-08 17:46:59 +0800

    修改判断网络方法

--- commonlib/src/main/java/com/zzy/commonlib/util/NetUtils.java
+++ commonlib/src/main/java/com/zzy/commonlib/util/NetUtils.java
@@ -20,22 +20,9 @@ public class NetUtils {
      * @return
      */
     public static boolean isNetworkAvailable(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cm == null) {
-        } else {
-            // 　　　　　　如果仅仅是用来判断网络连接
-            // 　　　　　　则可以使用 cm.getActiveNetworkInfo().isAvailable();
-            NetworkInfo[] info = cm.getAllNetworkInfo();
-            if (info != null) {
-                for (int i = 0; i < info.length; i++) {
-                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return (info != null && info.isAvailable());
     }
 
     /**
