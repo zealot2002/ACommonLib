217f6a907fb8a8fdc5004002b3f4e2dbfdf12c8c
zzy <zzy_2002@126.com>
2018-08-14 18:19:19 +0800

    add log util

---
+++ commonlib/src/main/java/com/zzy/commonlib/log/MyLog.java
@@ -0,0 +1,56 @@
+package com.zzy.commonlib.log;
+import android.util.Log;
+
+/**
+ * @author zzy
+ * @date 2018/8/14
+ */
+
+public class MyLog {
+    private static boolean enable = true;
+    public static void e(String tag, String s){
+        if(enable)
+            Log.e(tag,s);
+    }
+
+    public static void w(String tag, String s){
+        if(enable)
+            Log.w(tag,s);
+    }
+    public static void d(String tag, String s){
+        if(enable)
+            Log.d(tag,s);
+    }
+    public static void e(String s){
+        if(!enable) return;
+
+        String tag = generateTag();
+        Log.e(tag, s);
+    }
+    public static void w(String s){
+        if(!enable) return;
+
+        String tag = generateTag();
+        Log.w(tag, s);
+    }
+    public static void d(String s){
+        if(!enable) return;
+
+        String tag = generateTag();
+        Log.d(tag, s);
+    }
+
+    public static void setLogEnable(boolean b){
+        enable = b;
+    }
+
+    private static String generateTag() {
+        StackTraceElement caller = new Throwable().getStackTrace()[2];
+        String tag = "%s.%s(L:%d)";
+        String callerClazzName = caller.getClassName();
+        callerClazzName = callerClazzName.substring(callerClazzName.lastIndexOf(".") + 1);
+        tag = String.format(tag, callerClazzName, caller.getMethodName(), caller.getLineNumber());
+        return tag;
+    }
+
+}
