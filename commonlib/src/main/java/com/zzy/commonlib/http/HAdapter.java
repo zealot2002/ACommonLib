f2ab38cff3caa790996d71a917a413bb036a75a0
zealot2002 <zzy_2002@126.com>
2019-06-26 16:00:14 +0800

    update http lib 2

--- commonlib/src/main/java/com/zzy/commonlib/http/HAdapter.java
+++ commonlib/src/main/java/com/zzy/commonlib/http/HAdapter.java
@@ -83,7 +83,6 @@ public class HAdapter {
         try{
             responseTime = Long.valueOf(response.header("X-Timestamp"));
         }catch (Exception e){
-            e.printStackTrace();
         }
 
         return handleServerData(response.body().string(),response.code(),responseTime,ctx);
