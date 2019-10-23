dce29efde64431583e45e125739265541a0b7bd3
zealot2002 <zzy_2002@126.com>
2019-10-23 09:51:49 +0800

    open logcat

--- commonlib/src/main/java/com/zzy/commonlib/log/LogcatHelper.java
+++ commonlib/src/main/java/com/zzy/commonlib/log/LogcatHelper.java
@@ -102,9 +102,9 @@ public class LogcatHelper {
 			 * 
 			 * */
 			// cmds = "logcat *:e *:w | grep \"(" + mPID + ")\"";
-			// cmds = "logcat | grep \"(" + mPID + ")\"";//打印所有日志信息
+			 cmds = "logcat | grep \"(" + mPID + ")\"";//打印所有日志信息
 			// cmds = "logcat -s way";//打印标签过滤信息
-			cmds = "logcat *:e *:i | grep \"(" + mPID + ")\"";
+//			cmds = "logcat *:e *:i | grep \"(" + mPID + ")\"";
 		}
 
 		public void stopLogs() {
